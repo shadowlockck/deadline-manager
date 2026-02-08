9d8603a3e02df81a6b91febd2120aca944dffcc4
ShadowLock <bodia.petrik@gmail.com>
2026-02-08 12:40:58 +0200

    commit 2: Додано кнопку "Створитти задачу" яка покищо тільки перевіряє на чи заповнена форма створення

--- code/sources/ui.py
+++ code/sources/ui.py
@@ -2,35 +2,65 @@ import flet as ft
 import datetime
 
 
-
 def main(page: ft.Page):
     page.title = "Task manager"
     page.theme_mode = "dark"
 
+    selected_deadline = None 
+
     def date_changed(e):
-        if date_picker.value:
-            date_text.value = f"Дедайн через {(date_picker.value.date() - datetime.date.today()).days} днів"
-            page.update()
+        nonlocal selected_deadline
+        selected_deadline = date_picker.value
+        page.update()
 
     def open_picker(e):
         date_picker.open = True
         page.update()
 
 
-    date_text = ft.Text("Дата не вибрана")
+    def task_created(e):
+        if not name_of_task.value.strip() and selected_deadline is None:
+            error.value = "Введи назву задачі і вибери дедлайн"
+            
+            page.update()
+            return
 
+        elif selected_deadline is None:
+            error.value = "Спочатку вибери дедлайн"
+            page.update()
+            return
+            
+        elif not name_of_task.value.strip():
+            error.value = "Введи назву задачі"
+            page.update()
+            return
+        
+        error.value = "Завдання Успішно створено!"
+        error.color = "green"
+        
+
+
+
+    name_of_task = ft.TextField(label="Назва задачі")
 
     date_picker = ft.DatePicker(
         first_date=datetime.datetime.now(),
-        on_change=lambda e: date_changed(e)
+        last_date=datetime.datetime(2030, 12, 31),
+        on_change=date_changed
     )
 
     page.overlay.append(date_picker)
 
 
-    btn = ft.ElevatedButton("Вибрати дату", on_click=open_picker)
+    pick_btn = ft.ElevatedButton("Вибрати дедлайн", on_click=open_picker)
+    create_btn = ft.ElevatedButton("Створити задачу", on_click=task_created)
+    error = ft.Text("", color="red")
+    
 
-    page.add(btn, date_text)
+    page.add(
+        name_of_task,
+        pick_btn,
+        create_btn,
+        error,
+    )
 
-
-ft.app(target=main)
