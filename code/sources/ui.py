ec5ce7f02cbc4b394d2d891928af8481297153ee
ShadowLock <bodia.petrik@gmail.com>
2026-02-09 18:11:40 +0200

    Commit 5: Підтянутий UI, доданий вивід данних з json, додані не робочі(поки що) дропбокси для фільтрації та сортування списку завдань

--- code/sources/ui.py
+++ code/sources/ui.py
@@ -18,6 +18,34 @@ def main(page: ft.Page):
         date_picker.open = True
         page.update()
 
+    def load_tasks():
+        tasks_column.controls.clear()
+
+        db = tasks.import_db()
+
+        for task in db:
+            name = task["name"]
+            deadline = task["deadline"]
+            priority_value = task["priority"]
+
+            deadline_date = datetime.datetime.fromisoformat(deadline).date()
+            days = (deadline_date - datetime.date.today()).days
+
+            if days < 0:
+                subtitle = f"Прострочено на {abs(days)} днів | {priority_value}"
+            else:
+                subtitle = f"{days} днів | {priority_value}"
+
+            tile = ft.ListTile(
+                title=ft.Text(name),
+                subtitle=ft.Text(subtitle)
+            )
+
+            tasks_column.controls.append(tile)
+
+        page.update()
+
+
 
     def task_created(e):
         no_name = not name_of_task.value.strip()
@@ -28,6 +56,7 @@ def main(page: ft.Page):
 
             case (True, True, True):
                 error.value = "Введи назву задачі, вибери дедлайн та пріоритет"
+                name_of_task.border_color = "red"
                 return
 
             case (True, True, False):
@@ -44,6 +73,7 @@ def main(page: ft.Page):
 
             case (True, False, False):
                 error.value = "Введи назву задачі"
+                name_of_task.border_color = "red"
                 return
 
             case (False, True, False):
@@ -59,12 +89,12 @@ def main(page: ft.Page):
                 page.update()
 
         
-        error.value = "Завдання Успішно створено!"
+        error.value = "Завдання Успішно створено!",
         error.color = "green"
 
         tasks.create_task(name_of_task.value, selected_deadline.strftime("%Y-%m-%d"), priority.value)
+        load_tasks()
         
-
 
 
     name_of_task = ft.TextField(label="Назва задачі")
@@ -87,17 +117,88 @@ def main(page: ft.Page):
             ft.dropdown.Option("P1"),
         ],
         label="Пріоритет"
-
-
     )
+
+    sort_tasks = ft.Dropdown(
+        options=[
+            ft.dropdown.Option("За назвою"),
+            ft.dropdown.Option("За дедлайном"),
+            ft.dropdown.Option("За пріоритетом"),
+        ],
+        label="Сортувати задачі"
+    )
+
+    filter_tasks = ft.Dropdown(
+        options=[
+            ft.dropdown.Option("Всі задачі"),
+            ft.dropdown.Option("Прострочені"),
+            ],
+        label="Сортувати задачі"
+    )
+
     error = ft.Text("", color="red")
-    
+    tasks_column = ft.Column(
+        scroll="auto",
+        expand=True,
+        spacing=10
+    )
+
+
+    sort_tasks.width=150
+    filter_tasks.width=150
+
+    left_panel = ft.Container(
+        content=ft.Column(
+            [
+                ft.Row(
+                    [
+                        ft.Container(sort_tasks, expand=True),
+                        ft.Container(filter_tasks, expand=True),
+                    ],
+                    spacing=10
+                ),
+                ft.Divider(),
+                tasks_column
+            ],
+            expand=True,
+            spacing=10
+        ),
+        width=350,
+        padding=15,
+    )
+
+
+    create_panel = ft.Container(
+        content=ft.Column(
+            [
+                name_of_task,
+                pick_btn,
+                create_btn,
+                error,
+            ],
+            spacing=15,
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+            alignment=ft.MainAxisAlignment.CENTER
+        ),
+        expand=True,
+        alignment=ft.Alignment.CENTER,
+        padding=40
+    )
+
+
 
     page.add(
-        name_of_task,
-        pick_btn,
-        priority,
-        create_btn,
-        error,
+    ft.Row(
+        [
+            left_panel,
+            ft.VerticalDivider(width=1),
+            create_panel
+        ],
+        expand=True
     )
+)
 
+
+    
+
+    load_tasks()
